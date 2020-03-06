933930063e5cb920c31bf82a731d475e128d9fe9
Alina Kazakova <alinakazakova@live.ie>
2020-03-06 23:05:39 +0000

    delete account created;

---
+++ src/actions/DeleteAccount.java
@@ -0,0 +1,23 @@
+package actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import classes.Customer;
+import classes.CustomerAccount;
+
+public class DeleteAccount extends JFrame implements ActionListener {
+
+	public DeleteAccount(Customer c, CustomerAccount a) {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

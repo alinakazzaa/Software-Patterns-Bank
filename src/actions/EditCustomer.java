546505c836a5ed81e2a5c1005a1843b0f1a7ea87
Alina Kazakova <alinakazakova@live.ie>
2020-03-07 00:44:12 +0000

    edit customer

--- src/actions/EditCustomer.java
+++ src/actions/EditCustomer.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,7 +17,6 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-import banking.BankingMain;
 import classes.Customer;
 import menus.AdminMenu;
 

a5569498352c4b11ead31da570f737a10b91c6dd
Alina Kazakova <alinakazakova@live.ie>
2020-03-08 19:42:51 +0000

    customer menu log in working

--- src/menus/CustomerMenu.java
+++ src/menus/CustomerMenu.java
@@ -1,215 +1,197 @@
 package menus;
 
-import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 
+import actions.AccountScreen;
 import banking.BankingMain;
 import classes.Customer;
 import classes.CustomerAccount;
 
-public class CustomerMenu extends JFrame {
+public class CustomerMenu extends JFrame implements ActionListener {
 
+	private static CustomerMenu cusMenu;
 	String userType;
-	JFrame f1;
-	JLabel firstNameLabel, surnameLabel, pPPSLabel, dOBLabel;
-	JTextField firstNameTextField, surnameTextField, pPSTextField, dOBTextField;
+	JFrame f;
 	JPanel panel2;
-	JButton add;
+	JButton add, returnButton, continueButton;
+	Container content;
+	JPanel buttonPanel, boxPanel, labelPanel;
+	JLabel label;
 	
-	private StartMenu start = new StartMenu();
-	private BankingMain main = BankingMain.getInstance();
+	private StartMenu start;
+	private BankingMain main;
+	private Customer customer;
+	private CustomerAccount acc;
+	JComboBox<String> box;
 
-	public CustomerMenu(String userType) {
-		this.userType = userType;
-
-		if (userType.equals("New Customer")) {
-			newCustomer();
+	public CustomerMenu() {
+		main = BankingMain.getInstance();
+		start = new StartMenu();
+		
+		if(customer == null) {
+			if(customerLogIn()) {
+				customerMenuCreated();
+			}
 		} else {
-			existingCustomer();
+			customerMenuCreated();
 		}
+		
+		System.out.println(this.customer);
+		
+	}
+	
+	public static CustomerMenu getInstance() { // avoid log in each time window is called from another action - singleton
+		
+		if(cusMenu == null) {
+			cusMenu = new CustomerMenu();
+		}
+		
+		return cusMenu;
 	}
 
-	public void newCustomer() {
+	public void customerMenuCreated() {
 
-		f1 = new JFrame("Create New Customer");
-		f1.setSize(400, 300);
-		f1.setLocation(200, 200);
-		f1.addWindowListener(new WindowAdapter() {
+		f = new JFrame("Customer Menu");
+		f.setSize(400, 300);
+		f.setLocation(200, 200);
+		f.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent we) {
 				System.exit(0);
 			}
 		});
+		f.setVisible(true);
 
-		Container content = f1.getContentPane();
-		content.setLayout(new BorderLayout());
+		if (customer.getAccounts().size() == 0) {
+			JOptionPane.showMessageDialog(f,
+					"This customer does not have any accounts yet. \n An admin must create an account for this customer \n for them to be able to use customer functionality. ",
+					"Oops!", JOptionPane.INFORMATION_MESSAGE);
+			f.dispose();
+			start.menuStart();
+		} else {
+			buttonPanel = new JPanel();
+			boxPanel = new JPanel();
+			labelPanel = new JPanel();
 
-		firstNameLabel = new JLabel("First Name:", SwingConstants.RIGHT);
-		surnameLabel = new JLabel("Surname:", SwingConstants.RIGHT);
-		pPPSLabel = new JLabel("PPS Number:", SwingConstants.RIGHT);
-		dOBLabel = new JLabel("Date of birth", SwingConstants.RIGHT);
-		firstNameTextField = new JTextField(20);
-		surnameTextField = new JTextField(20);
-		pPSTextField = new JTextField(20);
-		dOBTextField = new JTextField(20);
-		JPanel panel = new JPanel(new GridLayout(6, 2));
-		panel.add(firstNameLabel);
-		panel.add(firstNameTextField);
-		panel.add(surnameLabel);
-		panel.add(surnameTextField);
-		panel.add(pPPSLabel);
-		panel.add(pPSTextField);
-		panel.add(dOBLabel);
-		panel.add(dOBTextField);
+			label = new JLabel("Select Account:");
+			labelPanel.add(label);
 
-		panel2 = new JPanel();
-		add = new JButton("Add");
-		add.addActionListener(new NewCustomer());
+			returnButton = new JButton("Return");
+			returnButton.addActionListener(this);
+			buttonPanel.add(returnButton);
+			
+			continueButton = new JButton("Continue");
+			continueButton.addActionListener(this);
+			buttonPanel.add(continueButton);
 
-		JButton cancel = new JButton("Cancel");
-		cancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				f1.dispose();
-				start.menuStart();
+			box = new JComboBox<String>();
+			
+			for (int i = 0; i < customer.getAccounts().size(); i++) {
+				box.addItem(customer.getAccounts().get(i).getNumber());
 			}
-		});
 
-		panel2.add(add);
-		panel2.add(cancel);
+			for (int i = 0; i < customer.getAccounts().size(); i++) {
+				if (customer.getAccounts().get(i).getNumber() == box.getSelectedItem()) {
+					acc = customer.getAccounts().get(i);
+				}
+			}
 
-		content.add(panel, BorderLayout.CENTER);
-		content.add(panel2, BorderLayout.SOUTH);
-
-		f1.setVisible(true);
-
+			boxPanel.add(box);
+			content = f.getContentPane();
+			content.setLayout(new GridLayout(3, 1));
+			content.add(labelPanel);
+			content.add(boxPanel);
+			content.add(buttonPanel);
+		}
 	}
 
-	public void existingCustomer() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		f.dispose();
+		if(e.getActionCommand().equals("Return")) {
+			// sign out user from state - ie set to null
+			setCustomer(null);
+		} else {
+			new AccountScreen(acc);
+		}
 		
+	}
+	
+	public boolean customerLogIn() {
 		boolean loop = true, loop2 = true;
 		boolean cont = false;
 		boolean found = false;
-		System.out.println(main.getCustomers());
 		Customer customer = null;
 		
-		while (loop) {
-			Object customerID = JOptionPane.showInputDialog(f1, "Enter Customer ID:");
-
-			if(main.getCustomers().size() > 0) {
-				for (Customer aCustomer : main.getCustomers()) {
-
-					if (aCustomer.getCustomerID().equals(customerID)) // search customer list for matching customer ID
-					{
-						found = true;
-						customer = aCustomer;
-					}
-				}
-
-				if (found == false) {
-					int reply = JOptionPane.showConfirmDialog(null, null, "User not found. Try again?",
-							JOptionPane.YES_NO_OPTION);
-					if (reply == JOptionPane.YES_OPTION) {
-						loop = true;
-					} else if (reply == JOptionPane.NO_OPTION) {
-						loop = false;
-						loop2 = false;
-						start.menuStart();
-					}
-				} else {
-					loop = false;
-				}
-				
-			} else {
-				int reply = JOptionPane.showConfirmDialog(null, null, "User not found. Try again?",
-						JOptionPane.YES_NO_OPTION);
-				if (reply == JOptionPane.YES_OPTION) {
-					loop = true;
-				} else if (reply == JOptionPane.NO_OPTION) {
-					loop = false;
-					loop2 = false;
-					start.menuStart();
-				}
-			}
-
-
-		}
-
-		while (loop2) {
-			Object customerPassword = JOptionPane.showInputDialog(f1, "Enter Customer Password;");
-
-			if (!customer.getPassword().equals(customerPassword))// check if custoemr password is correct
-			{
-				int reply = JOptionPane.showConfirmDialog(null, null, "Incorrect password. Try again?",
-						JOptionPane.YES_NO_OPTION);
-				if (reply == JOptionPane.YES_OPTION) {
-
-				} else if (reply == JOptionPane.NO_OPTION) {
-					loop2 = false;
-					start.menuStart();
-				}
-			} else {
-				loop2 = false;
-				cont = true;
-			}
-		}
-
-		if (cont) {
-			loop2 = false;
-			loop = false;
-			System.out.println("Show customer menu");
-		}
-	}
-
-	private class NewCustomer implements ActionListener {
-		String PPS, firstName, surname, DOB, CustomerID, password;
-
-		public void actionPerformed(ActionEvent e) {
-
-			PPS = pPSTextField.getText();
-			firstName = firstNameTextField.getText();
-			surname = surnameTextField.getText();
-			DOB = dOBTextField.getText();
-			password = "";
-			CustomerID = "ID" + PPS;
-
-			boolean loop = true;
-
-			while (loop) {
-				password = JOptionPane.showInputDialog(f1, "Enter 7 character Password;");
-
-				if (password.length() != 7) // Making sure password is 7 characters
-				{
-					JOptionPane.showMessageDialog(null, null, "Password must be 7 charatcers long",
-							JOptionPane.OK_OPTION);
-				} else {
-					loop = false;
-				}
-			}
+		if(main.getCustomers().size() > 0) {
 			
-			ArrayList<CustomerAccount> accounts = new ArrayList<CustomerAccount>();
-			Customer customer = new Customer(PPS, surname, firstName, DOB, CustomerID, password, accounts);
+			while (loop) {
+				Object customerID = JOptionPane.showInputDialog(f, "Enter Customer ID:");
+				customer = main.getCustomerByID(customerID.toString());
+				
+					if(customer == null) {
+						int reply = JOptionPane.showConfirmDialog(null, null, "User not found. Try again?",
+								JOptionPane.YES_NO_OPTION);
+						if (reply == JOptionPane.YES_OPTION) {
+							loop = true;
+						} else if (reply == JOptionPane.NO_OPTION) {
+							loop = false;
+							loop2 = false;
+							start.menuStart();
+						}
+					} else {
+						loop = false;
+						
+						while (loop2) {
+							Object customerPassword = JOptionPane.showInputDialog(f, "Enter Customer Password;");
+							
+							if (!customer.getPassword().equals(customerPassword))// check if custoemr password is correct
+							{
+								int reply = JOptionPane.showConfirmDialog(null, null, "Incorrect password. Try again?",
+										JOptionPane.YES_NO_OPTION);
+								if (reply == JOptionPane.YES_OPTION) {
 
-			main.addCustomer(customer);
+								} else if (reply == JOptionPane.NO_OPTION) {
+									loop2 = false;
+									start.menuStart();
+								}
+							} else {
+								loop2 = false;
+								cont = true;
+							}
+						}
 
-			JOptionPane.showMessageDialog(f1, "CustomerID = " + CustomerID + "\n Password = " + password,
-					"Customer created.", JOptionPane.INFORMATION_MESSAGE);
-			f1.dispose();
-			start.menuStart();
+						if (cont) {
+							found = true;
+							setCustomer(customer);
+						}
+					}
+
+			}
 		}
-
+		
+		return found;
+		
 	}
 	
+	// state pattern
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+	public Customer getCustomer() {
+		return this.customer;
+	}
+
 }

478f80e9b558c5c13579f5c1216f2e5259ae1518
Alina Kazakova <alinakazakova@live.ie>
2020-03-09 09:21:31 +0000

    one JFrame in Main class

--- src/banking/BankingMain.java
+++ src/banking/BankingMain.java
@@ -1,14 +1,19 @@
 package banking;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
+
+import javax.swing.JFrame;
 
 import classes.Customer;
 import menus.StartMenu;
 
-public class BankingMain {
+public class BankingMain extends JFrame {
 	
 	private ArrayList<Customer> customerList = new ArrayList<Customer>();
 	private static BankingMain main;
+	JFrame f;
 	
 	public static BankingMain getInstance() {
 		
@@ -57,4 +62,19 @@ public class BankingMain {
 	  }  
 	  return true;  
 	}
+	
+	public void createFrame(String title) {
+		
+		f = new JFrame(title);
+		f.setSize(400, 300);
+		f.setLocation(200, 200);
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) { System.exit(0); }
+		});
+	}
+	
+	public JFrame getFrame() {
+		return this.f;
+	}
+	
 }
